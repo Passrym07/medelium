66a287d4459cca0831d5399a4963c85addd22fe8
Léo <passrym07@gmail.com>
2022-10-28 16:18:16 +0200

    Commit

---
+++ src/main/java/me/passrym07/medelium/Medelium.java
@@ -0,0 +1,17 @@
+package me.passrym07.medelium;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Medelium extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
